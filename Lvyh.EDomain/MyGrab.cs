81170854ae35cd4593c9d7744f2ddaa493fff9c9
lvyh91 <979954999@qq.com>
2017-11-28 23:43:19 +0800

    v1.1

---
+++ Lvyh.EDomain/MyGrab.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace Lvyh.EDomain
+{
+   public class MyGrab
+    {
+       public static List<string> GetContents(string url)
+       {
+           List<string> titles = null;
+
+           //第一句，抓
+           XElement xml = XElement.Load(url);
+
+           //第二句，取
+           //string txt = "------------ 数学大冒险 -------------" + "\r\n";
+           titles = xml.Element("channel").Elements("item")
+                         .Select((ele) => ele.Element("title").Value)
+                         .ToList();
+           //.Select((m, index1) => txt += index1.ToString() + ":" + m.Element("title").Value + "\r\n")
+           //.Where((n, index2) => index2 < 5)
+           //.ToList();
+
+           //
+           return titles;
+
+       }
+    }
+}
